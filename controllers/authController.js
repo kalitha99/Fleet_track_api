872ee7d43bb66771b1baf55300076ba8f85ba0e3
Kalitha99 <kalithadissanayake44@gmail.com>
2022-09-13 23:49:30 +0530

    Auth model refac

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require("../models/userModel")
 const jwt = require('jsonwebtoken')
 require("dotenv").config();
+sendEmail = require("../send_mail/Send_mail")
 
 const handleSignup = async (req, res) => {
     console.log(req.body)
@@ -9,13 +10,43 @@ const handleSignup = async (req, res) => {
         await User.create({
             name: req.body.name,
             password: req.body.password,
-            role: req.body.role
+            role: req.body.role,
+            email: req.body.email
         })
         console.log("new user created")
-        res.json({status: 'ok'})
+        let to = req.body.email
+        let subject = "New user credentials"
+        let msg = "For loging in  to system please use these credentials and once you login change your password." + " USER NAME : " + req.body.name + "  PASSWORD : " + req.body.password + "."
+        sendEmail.sendingMail(to, subject, msg)
+        res.json({status: 'ok', msg: "account created"})
     } catch (err) {
         console.log(err)
         res.json({status: 'error', error: 'Duplicate email'})
+    }
+
+}
+const updatePw = async (req, res) => {
+    console.log(req.body)
+    try {
+    const user = await User.findOneAndUpdate({
+            name: req.body.name,
+        },
+        {
+            $set: {
+                password:req.body.password
+
+            }
+        },
+        {new: true, useFindAndModify: false}
+    )
+        res.json({status: 'ok', msg: 'Updated'})
+        let to = user.email
+        let subject = "User credentials updated"
+        let msg = "User credentials of this account has benn updated if ti was not you please contact admin "
+        sendEmail.sendingMail(to, subject, msg)
+    } catch (err) {
+        console.log(err)
+        res.json({status: 'error', msg: 'Duplicate email'})
     }
 
 }
@@ -30,6 +61,7 @@ const handleLogin = async (req, res) => {
         console.log(user)
         const accessToken = jwt.sign({
                 name: user.name,
+                email: user.email,
                 role: user.role
             }, process.env.ACCESS_TOKEN_SECRET,
             {
@@ -44,6 +76,7 @@ const handleLogin = async (req, res) => {
         res.json({
             status: "ok",
             name: user.name,
+            email: user.email,
             accessToken: accessToken,
             refreshToken: refreshToken,
             roles: user.role
@@ -51,7 +84,7 @@ const handleLogin = async (req, res) => {
         console.log("success")
     } else {
 
-        res.status(409).json({ msg: 'wrong user name or password'});
+        res.status(409).json({msg: 'wrong user name or password'});
         console.log("error")
     }
 
@@ -59,5 +92,4 @@ const handleLogin = async (req, res) => {
 }
 
 
-
-module.exports = {handleSignup, handleLogin};
+module.exports = {handleSignup, handleLogin, updatePw};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/authController');
+const verifyJWT =require('../../middelware/verifyJWT')
+const verifyRoles =require('../../middelware/verifyRoles')
 
 router.post('/login', authController.handleLogin);
+router.post('/handleSignup', verifyJWT, verifyRoles('admin'), authController.handleSignup);
+router.post('/updatePw', verifyJWT, verifyRoles('driver'), authController.updatePw);
 
 
 module.exports = router;
